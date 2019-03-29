df68ac47f1c823f17265b92f90e386edb656caef
luxiaoman <lufeng0614@163.com>
2019-03-29 14:59:31 +0800

    Create leetcode-28.py

---
+++ leetcode-28.py
@@ -0,0 +1,43 @@
+实现 strStr() 函数。
+
+给定一个 haystack 字符串和一个 needle 字符串，在 haystack 字符串中找出 needle 字符串出现的第一个位置 (从0开始)。如果不存在，则返回  -1。
+
+示例 1:
+
+输入: haystack = "hello", needle = "ll"
+输出: 2
+示例 2:
+
+输入: haystack = "aaaaa", needle = "bba"
+输出: -1
+说明:
+
+当 needle 是空字符串时，我们应当返回什么值呢？这是一个在面试中很好的问题。
+
+对于本题而言，当 needle 是空字符串时我们应当返回 0 。这与C语言的 strstr() 以及 Java的 indexOf() 定义相符。
+
+==================================================================================================
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if len(needle)==0:
+            return 0
+        if haystack.__contains__(needle):  ============
+            return haystack.index(needle)  ===========
+        return -1
+
+===========================================================
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if len(needle) == 0: return 0
+        elif len(haystack) < len(needle): return -1
+        elif haystack == needle: return 0
+        else:
+            l_needle = len(needle)
+            l_haystrack = len(haystack)
+            l_diff = l_haystrack - l_needle
+            for l in range(l_diff + 1):
+                end_l = l + l_needle
+                if haystack[l:end_l] == needle:
+                    return l
+            return -1
